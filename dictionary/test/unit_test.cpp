5d74cbe0c3152862718e9803fc288dd186d59579
m^f <53840135+MonsoonTycoon@users.noreply.github.com>
2019-11-28 21:49:40 -0800

    Updated/Finalized testing

--- dictionary/test/unit_test.cpp
+++ dictionary/test/unit_test.cpp
@@ -70,25 +70,138 @@ TEST(dict, array_dict_add_get) {
     ASSERT_EQ(val, 4);
 }
 
-TEST(dict, contains) {
+TEST(dict, containsSet1) {
     ArrayDictionary<int, int> hashTable;
 
-    hashTable.add(2, 9);
-    hashTable.add(3, 4);
-    hashTable.add(12, 8);
-    ASSERT_TRUE(hashTable.contain(2));
-    ASSERT_TRUE(hashTable.contain(3));
-    ASSERT_TRUE(hashTable.contain(12));
+    ASSERT_FALSE(hashTable.contain(-1));
+    ASSERT_FALSE(hashTable.contain(0));
+    ASSERT_FALSE(hashTable.contain(1));
 }
 
-TEST(dict, remove) {
+TEST(dict, containsSet2) {
+    ArrayDictionary<int, int> hashTable (1);
+
+    hashTable.add(0, 103);
+    ASSERT_FALSE(hashTable.contain(2));
+    ASSERT_TRUE(hashTable.contain(0));
+}
+
+TEST(dict, containsSet3) {
+    ArrayDictionary<int, int> hashTable (2);
+
+    hashTable.add(0, 103);
+    hashTable.add(1, 105);
+    ASSERT_TRUE(hashTable.contain(0));
+    ASSERT_TRUE(hashTable.contain(1));
+    ASSERT_FALSE(hashTable.contain(2));
+    ASSERT_FALSE(hashTable.contain(3));
+}
+
+TEST(dict, containsSet4) {
+    ArrayDictionary<int, int> hashTable (3);
+
+    hashTable.add(0, 103);
+    hashTable.add(1, 105);
+    ASSERT_FALSE(hashTable.contain(3));
+    hashTable.add(2, 206);
+    ASSERT_TRUE(hashTable.contain(1));
+}
+
+TEST(dict, containsSet5) {
+    ArrayDictionary<int, int> hashTable (3);
+
+    hashTable.add(0, 103);
+    hashTable.add(1, 105);
+    hashTable.add(2, 206);
+    hashTable.add(4, 407);
+    ASSERT_TRUE(hashTable.contain(1));
+    ASSERT_TRUE(hashTable.contain(4));
+    ASSERT_FALSE(hashTable.contain(7));
+    ASSERT_FALSE(hashTable.contain(8));
+}
+
+TEST(dict, removeEmpty) {
     ArrayDictionary<int, int> hashTable;
 
-    hashTable.add(2, 9);
-    hashTable.add(3, 4);
-    hashTable.add(12, 8);
-    ASSERT_TRUE(hashTable.remove(2));
-    ASSERT_TRUE(hashTable.remove(3));
-    ASSERT_TRUE(hashTable.remove(12));
+    ASSERT_FALSE(hashTable.remove(-1));
+    ASSERT_FALSE(hashTable.remove(0));
+    ASSERT_FALSE(hashTable.remove(1));
+}
 
+TEST(dict, removeSingleElement1Cap) {
+    ArrayDictionary<int, int> hashTable (1);
+
+    hashTable.add(0, 9);
+    ASSERT_FALSE(hashTable.remove(3));
+    ASSERT_TRUE(hashTable.remove(0));
+}
+
+TEST(dict, removeSingleElement5Cap) {
+    ArrayDictionary<int, int> hashTable (5);
+
+    hashTable.add(3, 9);
+    ASSERT_FALSE(hashTable.remove(14));
+    ASSERT_TRUE(hashTable.remove(3));
+}
+
+TEST(dict, removeSingleElement1CapCollision) {
+    ArrayDictionary<int, int> hashTable (1);
+
+    hashTable.add(1, 9);
+    hashTable.add(0, 9);
+    ASSERT_FALSE(hashTable.remove(3));
+    ASSERT_TRUE(hashTable.remove(0));
+}
+
+TEST(dict, removeSingleElement5CapCollision) {
+    ArrayDictionary<int, int> hashTable (5);
+
+    hashTable.add(12, 9);
+    hashTable.add(3, 9);
+    ASSERT_FALSE(hashTable.remove(14));
+    ASSERT_TRUE(hashTable.remove(3));
+}
+
+TEST(dict, removeDoesNotExist) {
+    ArrayDictionary<int, int> hashTable (5);
+
+    hashTable.add(2, 9);
+    hashTable.add(3, 9);
+    hashTable.add(4, 9);
+    ASSERT_FALSE(hashTable.remove(1));
+    ASSERT_FALSE(hashTable.remove(5));
+}
+
+TEST(dict, remove10CapCollisionTest) {
+    ArrayDictionary<int, int> hashTable;
+
+    hashTable.add(1, 9);
+    hashTable.add(3, 9);
+    hashTable.add(13, 9);
+    hashTable.add(31, 9);
+    hashTable.add(33, 9);
+    hashTable.add(11, 9);
+    hashTable.add(21, 9);
+    hashTable.add(4, 9);
+    ASSERT_FALSE(hashTable.remove(14));
+    ASSERT_TRUE(hashTable.remove(11));
+    ASSERT_TRUE(hashTable.remove(21));
+    ASSERT_TRUE(hashTable.remove(1));
+}
+
+TEST(dict, remove10CapCollisionTestDNE) {
+    ArrayDictionary<int, int> hashTable;
+
+    hashTable.add(1, 9);
+    hashTable.add(3, 9);
+    hashTable.add(13, 9);
+    hashTable.add(31, 9);
+    hashTable.add(33, 9);
+    hashTable.add(11, 9);
+    hashTable.add(21, 9);
+    hashTable.add(4, 9);
+    ASSERT_FALSE(hashTable.remove(0));
+    ASSERT_FALSE(hashTable.remove(5));
+    ASSERT_FALSE(hashTable.remove(10));
+    ASSERT_FALSE(hashTable.remove(2));
 }
