69d1a4a7f4557f80a68b3fb0af296c539412c3ae
m^f <53840135+MonsoonTycoon@users.noreply.github.com>
2019-11-30 15:02:37 -0800

    solver updated

--- sudoku/src/sudoku.h
+++ sudoku/src/sudoku.h
@@ -55,7 +55,82 @@ public:
     }
 
     bool solve() {
-        // homework
-        return false;   // placeholder
+        return SolveSudoku( grid );
     }
+
+    bool unsolved(int grid[9][9],
+                                int &row, int &col) {
+        for (row = 0; row < 9; row++)
+            for (col = 0; col < 9; col++)
+                if (grid[row][col] == 0)
+                    return true;
+        return false;
+    }
+
+    bool isSafe(int grid[9][9], int row,
+                int col, int num) {
+        return !UsedInRow(grid, row, num) &&
+               !UsedInCol(grid, col, num) &&
+               !UsedInBox(grid, row - row % 3 ,
+                          col - col % 3, num) &&
+               grid[row][col] == 0;
+    }
+    
+    bool SolveSudoku(int grid[9][9]) {
+        int row, col;
+        // If there is no unassigned location,
+        // we are done
+        if (!unsolved(grid, row, col))
+            return true; // success!
+        // consider digits 1 to 9
+        for (int num = 1; num <= 9; num++)
+        {
+            // if looks promising
+            if (isSafe(grid, row, col, num))
+            {
+                // make tentative assignment
+                grid[row][col] = num;
+                // return, if success, yay!
+                if (SolveSudoku(grid))
+                    return true;
+                // failure, unmake & try again
+                grid[row][col] = 0;
+            }
+        }
+        return false; // this triggers backtracking
+    }
+
+/* Returns a boolean which indicates whether
+an assigned entry in the specified row matches
+the given number. */
+    bool UsedInRow(int grid[9][9], int row, int num) {
+        for (int col = 0; col < 9; col++)
+            if (grid[row][col] == num)
+                return true;
+        return false;
+    }
+
+/* Returns a boolean which indicates whether
+an assigned entry in the specified column
+matches the given number. */
+    bool UsedInCol(int grid[9][9], int col, int num) {
+        for (int row = 0; row < 9; row++)
+            if (grid[row][col] == num)
+                return true;
+        return false;
+    }
+
+/* Returns a boolean which indicates whether
+an assigned entry within the specified 3x3 box
+matches the given number. */
+    bool UsedInBox(int grid[9][9], int boxStartRow,
+                   int boxStartCol, int num) {
+        for (int row = 0; row < 3; row++)
+            for (int col = 0; col < 3; col++)
+                if (grid[row + boxStartRow]
+                    [col + boxStartCol] == num)
+                    return true;
+        return false;
+    }
+
 };
